76f1bb3515664c79cc798001ed27bdc69f1b1c32
higorsantana-omega <higor.santanahs17@gmail.com>
2023-07-09 19:58:32 -0300

    refactor: move validate ownership to new service

--- api/src/modules/bank-accounts/bank-accounts.service.ts
+++ api/src/modules/bank-accounts/bank-accounts.service.ts
@@ -1,12 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
 import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 import { BankAccountsRepository } from 'src/database/repositories/bank-accounts.repositories';
+import { ValidateBankAccountOwnershipService } from './validate-bank-account-ownership.service';
 
 @Injectable()
 export class BankAccountsService {
   constructor(
     private readonly bankAccountsRepository: BankAccountsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
   ) {}
 
   create(userId: string, createBankAccountDto: CreateBankAccountDto) {
@@ -25,7 +27,10 @@ export class BankAccountsService {
     bankAccountId: string,
     updateBankAccountDto: UpdateBankAccountDto,
   ) {
-    await this.validateBankAccountOwnership(userId, bankAccountId);
+    await this.validateBankAccountOwnershipService.validate(
+      userId,
+      bankAccountId,
+    );
 
     return this.bankAccountsRepository.update(
       bankAccountId,
@@ -34,22 +39,13 @@ export class BankAccountsService {
   }
 
   async remove(userId: string, bankAccountId: string) {
-    await this.validateBankAccountOwnership(userId, bankAccountId);
-
-    await this.bankAccountsRepository.delete(bankAccountId);
-
-    return null;
-  }
-
-  private async validateBankAccountOwnership(
-    userId: string,
-    bankAccountId: string,
-  ) {
-    const isOwner = await this.bankAccountsRepository.findFirst(
+    await this.validateBankAccountOwnershipService.validate(
       userId,
       bankAccountId,
     );
 
-    if (!isOwner) throw new NotFoundException('Bank account not found.');
+    await this.bankAccountsRepository.delete(bankAccountId);
+
+    return null;
   }
 }
