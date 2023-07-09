0144eb056adb9dbafeae195e5781e0adf2fc61dd
higorsantana-omega <higor.santanahs17@gmail.com>
2023-07-09 16:05:42 -0300

    refactor: move metadata public to main controller

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -4,17 +4,16 @@ import { AuthenticateDto } from './dto/authenticate.dto';
 import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
+@SetMetadata('IS_PUBLIC', true)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  @SetMetadata('IS_PUBLIC', true)
   signin(@Body() authenticateDto: AuthenticateDto) {
     return this.authService.authenticate(authenticateDto);
   }
 
   @Post('signup')
-  @SetMetadata('IS_PUBLIC', true)
   create(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
