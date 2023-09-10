16fd3d21ac846d7fad099da5ef8985e1420cadeb
higorsantana-omega <higor.santanahs17@gmail.com>
2023-09-10 16:16:27 -0300

    feat: dashboard page and slider accounts

---
+++ web/src/view/components/UserMenu.tsx
@@ -0,0 +1,7 @@
+export function UserMenu () {
+  return (
+    <div className='bg-teal-50 border-teal-100 rounded-full w-12 h-12 flex justify-center items-center'>
+      <span className='text-sm tracking-[-0.5px] text-teal-900 font-medium'>HS</span>
+    </div>
+  )
+}
