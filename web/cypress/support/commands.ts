73a0021038201bc899acb1100819d40157769bfb
higorsantana-omega <higor.santanahs17@gmail.com>
2023-09-24 22:31:41 -0300

    test: login page

--- web/cypress/support/commands.ts
+++ web/cypress/support/commands.ts
@@ -10,28 +10,19 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Cypress {
+  interface Chainable {
+    register(name: string, email: string, password: string): Chainable
+  }
+}
+
+Cypress.Commands.add('register', (name, email, password) => {
+  cy.visit('http://localhost:5173/register')
+
+  cy.get('#name').type(name as string)
+  cy.get('#email').type(email)
+  cy.get('#password').type(password)
+  cy.get('button').contains('Criar conta').click()
+})
